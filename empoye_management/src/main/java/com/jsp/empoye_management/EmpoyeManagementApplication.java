af1e5999e0a08277d38dd86dbfb4c90875632d9f
aliya <shaikaliya996@gmail.com>
2024-08-01 17:17:00 +0530

    uploading base project for employe management

---
+++ empoye_management/src/main/java/com/jsp/empoye_management/EmpoyeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.empoye_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpoyeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpoyeManagementApplication.class, args);
+	}
+
+}
